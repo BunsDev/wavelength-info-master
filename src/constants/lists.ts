4695d9c38004a4c9f45781cada1c94458e206cd3
Chimpy Developer <86309030+chimpydev@users.noreply.github.com>
2022-08-29 16:13:35 +0100

    Delete Arbitrum Tokens List

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,12 +1,10 @@
 // used to mark unsupported tokens, these are hosted lists of unsupported tokens
 
 export const UNSUPPORTED_LIST_URLS: string[] = [];
-export const ARBITRUM_LIST = 'https://bridge.arbitrum.io/token-list-42161.json';
 export const BEETHOVEN_LIST = 'https://raw.githubusercontent.com/wavelength-velas/assets/main/generated/listed.tokensinfo.json';
 
 // lower index == higher priority for token import
 export const DEFAULT_LIST_OF_LISTS: string[] = [
-    ARBITRUM_LIST,
     BEETHOVEN_LIST,
     ...UNSUPPORTED_LIST_URLS, // need to load unsupported tokens as well
 ];
